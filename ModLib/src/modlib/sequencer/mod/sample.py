87b304e2ac0718ed1b9f805a9a36e140f98a29c9
Knut Eldhuset <knut.eldhuset@gmail.com>
2009-05-08 11:01:41 +0200

    Implemented get_data that handles looping.

--- ModLib/src/modlib/sequencer/mod/sample.py
+++ ModLib/src/modlib/sequencer/mod/sample.py
@@ -8,3 +8,32 @@ class Sample(object):
         self.repeat_length = repeat_length
         self.data = data
         
+    def get_data(self, offset, length):
+        data = []
+        if offset < self.length:
+            if offset + length < self.length:
+                part = self.data[offset:offset + length]
+            else:
+                part = self.data[offset:]
+            data.append(part)
+            length -= len(part)
+            offset += len(part)
+        if self.repeat_length > 0 and length > 0:
+            offset -= self.length
+            while length > 0:
+                offset %= self.repeat_length
+                if offset + length < self.repeat_length:
+                    part = self.data[self.repeat_offset + offset:self.repeat_offset + offset + length]
+                else:
+                    part = self.data[self.repeat_offset + offset:self.repeat_offset + self.repeat_length]
+                data.append(part)
+                length -= len(part)
+                offset += len(part)
+        return "".join(data)
+    
+if __name__ == "__main__":
+    s = Sample("Jalla", 10, 0, 64, 4, 3, "aaaarepbbb")
+    print s.get_data(0, 5)
+    print s.get_data(5, 10)
+    print s.get_data(15, 22)
+        
