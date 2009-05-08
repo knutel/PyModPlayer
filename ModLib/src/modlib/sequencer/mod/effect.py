eecbca05a5f2babc32e284ef081c1ee45c7237f5
Knut Eldhuset <knut.eldhuset@gmail.com>
2009-05-08 11:00:55 +0200

    Made the effects give sensible string representations.

--- ModLib/src/modlib/sequencer/mod/effect.py
+++ ModLib/src/modlib/sequencer/mod/effect.py
@@ -1,99 +1,129 @@
         
 class ExtendedEffect(object):
-    def __init__(self, x):
+    def __init__(self, x, channel, sequencer):
         self.x = x
+        self.channel = channel
+        self.sequencer = sequencer
+        
+    def tick(self):
+        pass
+    
+    def __str__(self):
+        return "%X%X%X" % (14, self.id, self.x)
         
 class Effect(ExtendedEffect):
-    def __init__(self, x, y):
-        super(Effect, self).__init__(x)
+    def __init__(self, x, y, channel, sequencer):
+        super(Effect, self).__init__(x, channel, sequencer)
         self.y = y
+
+    def __str__(self):
+        return "%X%X%X" % (self.id, self.x, self.y)
         
 class Arpeggio(Effect):
-    pass
+    id = 0
 
 class SlideUp(Effect):
-    pass
+    id = 1
+    
+    def tick(self):
+        if self.sequencer.tick_counter != 0:
+            self.channel.period -= self.x * 16 + self.y
+            if self.channel.period < 113:
+                self.channel.period = 113
 
 class SlideDown(Effect):
-    pass
+    id = 2
 
 class SlideToNote(Effect):
-    pass
+    id = 3
 
 class Vibrato(Effect):
-    pass
+    id = 4
 
 class ContinueSlideToNotePlusVolumeSlide(Effect):
-    pass
+    id = 5
 
 class ContinueVibratoPlusVolumeSlide(Effect):
-    pass
+    id = 6
 
 class Tremolo(Effect):
-    pass
+    id = 7
 
 class SetSampleOffset(Effect):
-    pass
+    id = 9
 
 class VolumeSlide(Effect):
-    pass
+    id = 10
+
+    def tick(self):
+        if self.sequencer.tick_counter != 0:
+            if self.x > 0:
+                self.channel.volume = min((64, self.channel.volume + self.x))
+            else:
+                self.channel.volume = max((0, self.channel.volume - self.y))
 
 class PositionJump(Effect):
-    pass
+    id = 11
 
 class SetVolume(Effect):
-    pass
+    id = 12
 
 class PatternBreak(Effect):
-    pass
+    id = 13
 
 class SetSpeed(Effect):
-    pass
+    id = 15
+    
+    def __init__(self, x, y, channel, sequencer):
+        super(SetSpeed, self).__init__(x, y, channel, sequencer)
+        speed = x * 16 + y
+        if speed <= 32:
+            sequencer.ticks_per_division = speed
 
 class ToggleFilter(ExtendedEffect):
-    pass
+    id = 0
 
 class FineslideUp(ExtendedEffect):
-    pass
-
+    id = 1
+    
 class FineslideDown(ExtendedEffect):
-    pass
+    id = 2
 
 class ToggleGlissando(ExtendedEffect):
-    pass
+    id = 3
 
 class SetVibratoWaveform(ExtendedEffect):
-    pass
+    id = 4
 
 class SetFinetuneValue(ExtendedEffect):
-    pass
+    id = 5
 
 class LoopPattern(ExtendedEffect):
-    pass
+    id = 6
 
 class SetTremoloWaveform(ExtendedEffect):
-    pass
+    id = 7
 
 class RetriggerSample(ExtendedEffect):
-    pass
+    id = 9
 
 class FineVolumeSlideUp(ExtendedEffect):
-    pass
+    id = 10
 
 class FineVolumeSlideDown(ExtendedEffect):
-    pass
+    id = 11
 
 class CutSample(ExtendedEffect):
-    pass
+    id = 12
 
 class DelaySample(ExtendedEffect):
-    pass
+    id = 13
 
 class DelayPattern(ExtendedEffect):
-    pass
+    id = 14
 
 class InvertLoop(ExtendedEffect):
-    pass
+    id = 15
 
 effects = dict([(klass.__name__, klass) for klass in [Arpeggio,
                                                   SlideUp,
@@ -129,14 +159,12 @@ extended_effects = dict([(klass.__name__, klass) for klass in [ToggleFilter,
                                                            ]])
 
 def effect_factory(effect, x, y=None):
-    if effect == "Arpeggio" and x == 0 and y == 0:
-        return None
     if y is None:
-        return extended_effects[effect](x)
+        return lambda channel, sequencer: extended_effects[effect](x, channel, sequencer)
     else:
-        return effects[effect](x, y)
+        return lambda channel, sequencer: effects[effect](x, y, channel, sequencer)
     
 if __name__ == "__main__":
     print effect_factory("Arpeggio", 0, 0)
-    print effect_factory("SetSpeed", 4, 5)
-    print effect_factory("CutSample", 5)
+    print effect_factory("SetSpeed", 4, 5)(None, None)
+    print effect_factory("CutSample", 5)(None, None)
