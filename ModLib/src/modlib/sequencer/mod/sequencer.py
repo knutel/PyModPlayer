33baa90a351c4a3eda0e8e42fb169fe43a9c169c
Knut Eldhuset <knut.eldhuset@gmail.com>
2009-05-11 13:56:30 +0200

    Made properties out of some variables. Added new test code.

--- ModLib/src/modlib/sequencer/mod/sequencer.py
+++ ModLib/src/modlib/sequencer/mod/sequencer.py
@@ -75,19 +75,20 @@ class Sequencer(object):
         self.module = module
         self.sequence_index = -1
         self.division_index = 63
-        self.pattern_index = self.module.pattern_sequence[self.sequence_index]
         self.master_tick_counter = -1
         self.tick_counter = 5
         self.position_counter = -1
         
         self.system_clock = 7093789.2 #PAL for now
-        self.tick_time = 0.02 #PAL for now
+        self.tick_time = 0.02 #PAL for now, intial value only
         self.ticks_per_division = 6
         
         self.channels = [Channel(self) for n in range(4)]
 
         self.ended = False
         
+    pattern_index = property(lambda self: self.module.pattern_sequence[self.sequence_index])
+    pattern = property(lambda self: self.module.patterns[self.pattern_index])
         
     def tick(self):
         self.update_state()
@@ -107,55 +108,33 @@ class Sequencer(object):
                     self.division_index = 0
                     self.sequence_index += 1
                     self.position_counter += 1
-                    if self.position_counter == self.module.num_positions:
+                    if self.position_counter > self.module.num_positions:
                         self.ended = True
                         return
-                    self.pattern_index = self.module.pattern_sequence[self.sequence_index]
                     self.loop_pattern_counter = 0
                     self.loop_pattern_division_start = 0
                     self.loop_pattern_division_stop = 0
                 for index, channel in enumerate(self.channels):
-                    channel.new_note(self.module.patterns[self.pattern_index].divisions[self.division_index].channel_data[index])
+                    channel.new_note(self.pattern.divisions[self.division_index].channel_data[index])
                     
                 print "Sequence: %3i/%3i, Pattern: %3i, Division: %2i, %s" % (self.sequence_index, self.module.num_positions, self.pattern_index, self.division_index, [str(channel) for channel in self.channels])
             
 if __name__ == "__main__":
     from modlib.sequencer.mod.loader import load
-    from audioop import ratecv, mul, tostereo, add, bias
-    import wave
     
-    seq = Sequencer(load("/Volumes/Stuff/old_backup_cds/cd2/SCENE/Mod/MOUSEMOD.MOD"))
-    import time
-    f = wave.open("/tmp/test.wav", "wb")
-    f.setnchannels(1)
-    f.setsampwidth(1)
-    f.setframerate(44100)
-    state = None
-    output = ["", "", "", ""]
-    state = [None, None, None, None]
-    for n in range(4):
-        dummy, state[n] = ratecv("\x80"*100, 1, 1, 100*50, 44100, state[n])
-    print time.clock()
-    while True:
-        try:
-            sound = seq.tick()
-        except:
+    filenames = ["/Volumes/Stuff/old_backup_cds/cd2/SCENE/Mod/MOUSEMOD.MOD",
+        "/Volumes/Stuff/old_backup_cds/cd2/SCENE/Mod/NIM.MOD",
+        "/Volumes/Stuff/old_backup_cds/cd2/SCENE/Mod/NOWHAT.MOD",
+        "/Volumes/Stuff/old_backup_cds/cd2/SCENE/Mod2/OCEAN2ND.MOD",
+        "/Volumes/Stuff/old_backup_cds/cd2/SCENE/Mod2/OCEAN.MOD",
+        "/Volumes/Stuff/old_backup_cds/cd2/SCENE/Mod/CREATURE.MOD",
+        "/Volumes/Stuff/old_backup_cds/cd2/SCENE/Mod/ASSASIN.MOD",
+        "/Volumes/Stuff/old_backup_cds/cd2/SCENE/Mod2/COMIC3.MOD",
+        "/Volumes/Stuff/old_backup_cds/cd2/SCENE/Mod2/PARALLAX.MOD",
+        "/Volumes/Stuff/old_backup_cds/cd2/SCENE/Mod/RAINYDAY.MOD"]
+    
+    for filename in filenames:
+        print filename
+        seq = Sequencer(load(filename))
+        while seq.tick():
             pass
-        if sound is None:
-            break
-        for n in range(4):
-            while True:
-                output[n], state[n] = ratecv(sound[n], 1, 1, len(sound[n])*50, 44100, state[n])
-                if len(output[n]) == 44100 / 50:
-                    break
-            output[n] = mul(output[n], 1, 0.25)
-            #output[n] = mul(output[n], 1, 0.5)
-            #output[n] = tostereo(output[n], 1, n % 2, (n + 1) % 2)
-        #left = add(output[0], output[2], 1)
-        #right = add(output[1], output[3], 1)
-        #stereo = add(left, right, 1)
-        #f.writeframes(stereo)
-        f.writeframes(bias(add(add(output[0], output[1], 1), add(output[2], output[3], 1), 1), 1, 128))
-        #f.writeframes(bias(output[0], 1, 128))
-    print time.clock()
-    f.close()
